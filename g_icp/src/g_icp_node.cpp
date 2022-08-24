f03b64808d1e5b113418648d14785a9b43428575
Dong-Young-Kim <bigdykim@gmail.com>
2022-08-24 23:25:19 +0900

    mapping first

--- g_icp/src/g_icp_node.cpp
+++ g_icp/src/g_icp_node.cpp
@@ -17,63 +17,68 @@ ros::Publisher pub_res;
 bool first_frame = true;
 pcl::PointCloud<pcl::PointXYZI> src;
 pcl::PointCloud<pcl::PointXYZI> tgt;
+pcl::PointCloud<pcl::PointXYZI> tgtaligned;
+pcl::PointCloud<pcl::PointXYZI> mapaligned;
+
+Eigen::Matrix4f src2tgt;
+Eigen::Matrix4f tgt2map;
+double          fitnessScore;
+bool            hasConverged;
+
+pcl::PointCloud<pcl::PointXYZI> PCmap;
 
 using namespace std;
 
-void colorize(const pcl::PointCloud<pcl::PointXYZI> &pc, pcl::PointCloud<pcl::PointXYZRGB> &pc_colored, const vector<int> &color) {
-    int N = pc.points.size();
+// void colorize(const pcl::PointCloud<pcl::PointXYZI> &pc, pcl::PointCloud<pcl::PointXYZRGB> &pc_colored, const vector<int> &color) {
+//     int N = pc.points.size();
 
-    pc_colored.clear();
-    pcl::PointXYZRGB pt_tmp;
-    for (int i = 0; i < N; ++i) {
-        const auto &pt = pc.points[i];
-        pt_tmp.x = pt.x;
-        pt_tmp.y = pt.y;
-        pt_tmp.z = pt.z;
-        pt_tmp.r = color[0];
-        pt_tmp.g = color[1];
-        pt_tmp.b = color[2];
-        pc_colored.points.emplace_back(pt_tmp);
-    }
-}
+//     pc_colored.clear();
+//     pcl::PointXYZRGB pt_tmp;
+//     for (int i = 0; i < N; ++i) {
+//         const auto &pt = pc.points[i];
+//         pt_tmp.x = pt.x;
+//         pt_tmp.y = pt.y;
+//         pt_tmp.z = pt.z;
+//         pt_tmp.r = color[0];
+//         pt_tmp.g = color[1];
+//         pt_tmp.b = color[2];
+//         pc_colored.points.emplace_back(pt_tmp);
+//     }
+// }
 
 void g_icp(){
+    
+    //initialize
     pcl::GeneralizedIterativeClosestPoint<pcl::PointXYZI, pcl::PointXYZI> gicp;
     gicp.setMaxCorrespondenceDistance(1.0);
     gicp.setTransformationEpsilon(0.003);
-    gicp.setMaximumIterations(1000);
+    gicp.setMaximumIterations(500);
 
     pcl::PointCloud<pcl::PointXYZI>::Ptr align(new pcl::PointCloud<pcl::PointXYZI>);
     gicp.setInputSource(src.makeShared()); //make shared return heap/smart pointer
     gicp.setInputTarget(tgt.makeShared());
     gicp.align(*align);
+    tgtaligned = *align;
 
     // Set outputs
-    Eigen::Matrix4f src2tgt   = gicp.getFinalTransformation();
-    double score     = gicp.getFitnessScore();
-    bool is_converged = gicp.hasConverged();
+    src2tgt      = gicp.getFinalTransformation();
+    fitnessScore = gicp.getFitnessScore();
+    hasConverged = gicp.hasConverged();
 
     cout << "source to target matrix\n" << src2tgt << endl;
-    cout << "matched score " << score << endl << endl;
+    cout << "matched score " << fitnessScore << endl << endl;
 
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr src_colored(new pcl::PointCloud<pcl::PointXYZRGB>);
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr tgt_colored(new pcl::PointCloud<pcl::PointXYZRGB>);
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr align_colored(new pcl::PointCloud<pcl::PointXYZRGB>);
-    colorize(src, *src_colored, {255, 0, 0});
-    colorize(tgt, *tgt_colored, {0, 255, 0});
-    colorize(*align, *align_colored, {255, 0, 255});
+    //colerize
+    // pcl::PointCloud<pcl::PointXYZRGB>::Ptr src_colored(new pcl::PointCloud<pcl::PointXYZRGB>);
+    // pcl::PointCloud<pcl::PointXYZRGB>::Ptr tgt_colored(new pcl::PointCloud<pcl::PointXYZRGB>);
+    // pcl::PointCloud<pcl::PointXYZRGB>::Ptr align_colored(new pcl::PointCloud<pcl::PointXYZRGB>);
+    // colorize(src, *src_colored, {255, 0, 0});
+    // colorize(tgt, *tgt_colored, {0, 255, 0});
+    // colorize(*align, *align_colored, {255, 0, 255});
 
-    pcl::PointCloud<pcl::PointXYZRGB> pubCloud;
-    pubCloud = *tgt_colored + *align_colored;
-
-    sensor_msgs::PointCloud2 output;
-    pcl::PCLPointCloud2 tmp_PCL;                               //declare PCL_PC2
-    pcl::toPCLPointCloud2(pubCloud, tmp_PCL);                     //PC -> PCL_PC2
-    pcl_conversions::fromPCL(tmp_PCL, output);                 //PCL_PC2 -> sensor_msg_PC2
-    output.header.stamp = ros::Time::now();
-    output.header.frame_id = "map";
-
-    pub_res.publish(output);
+    //merge and publish
+    // pcl::PointCloud<pcl::PointXYZRGB> pubCloud;
+    // pubCloud = *tgt_colored + *align_colored;
 }
 
 void data_process(const sensor_msgs::PointCloud2ConstPtr& scan){
@@ -83,11 +88,14 @@ void data_process(const sensor_msgs::PointCloud2ConstPtr& scan){
 
     pcl::VoxelGrid<pcl::PointXYZI> vg;
     vg.setInputCloud(input_raw.makeShared());
-    vg.setLeafSize(0.08f, 0.08f, 0.08f);
+    vg.setLeafSize(0.15f, 0.15f, 0.15f);
     vg.filter(downsampled_cloud);
 
+    //if first frame
     if(first_frame) {
+        pcl::copyPointCloud(downsampled_cloud, PCmap);
         tgt = downsampled_cloud;
+        tgt2map = Eigen::Matrix4f::Identity();
         first_frame = false;
         return;
     }
@@ -95,14 +103,29 @@ void data_process(const sensor_msgs::PointCloud2ConstPtr& scan){
     //not first frame
     src = downsampled_cloud;
     g_icp();
-    //prev_src = crnt_tgt;
+    if(hasConverged && fitnessScore < 0.1){
+        pcl::transformPointCloud (tgtaligned, mapaligned, tgt2map);
+        tgt2map = tgt2map * src2tgt;
+        PCmap += mapaligned;
+        tgt = src;
+        cout << "point number of map  " << PCmap.size() << endl;
+        cout << "point number of mapaligned  " << mapaligned.size() << endl;
+    }
 
+    //publish
+    sensor_msgs::PointCloud2 output;
+    pcl::PCLPointCloud2 tmp_PCL;                               //declare PCL_PC2
+    pcl::toPCLPointCloud2(PCmap, tmp_PCL);                     //PC -> PCL_PC2
+    pcl_conversions::fromPCL(tmp_PCL, output);                 //PCL_PC2 -> sensor_msg_PC2
+    output.header.stamp = ros::Time::now();
+    output.header.frame_id = "map";
+    pub_res.publish(output);
 }
 
 int main(int argc, char**argv) {
     ros::init(argc, argv, "g_icp");             //node name 
 	ros::NodeHandle nh;      
-    ros::Subscriber sub = nh.subscribe<sensor_msgs::PointCloud2> ("/velodyne_points", 1, data_process);
+    ros::Subscriber sub = nh.subscribe<sensor_msgs::PointCloud2> ("/3_velodyne_points_Clustering", 1, data_process);
     pub_res = nh.advertise<sensor_msgs::PointCloud2> ("/merged_cloud", 1);
     
 
